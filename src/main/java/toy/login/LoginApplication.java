d1f16656641d1cb01aad3736de366df7c54c7a9a
IMWoo94 <leemeawoo94@gmail.com>
2023-10-31 17:02:31 +0900

    chore : 프로젝트 초기 세팅 init
    프로젝트 초기 세팅 처리
    gitCommitMessage apply
    필요 라이브러이 의존성 추가

---
+++ src/main/java/toy/login/LoginApplication.java
@@ -0,0 +1,17 @@
+package toy.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
