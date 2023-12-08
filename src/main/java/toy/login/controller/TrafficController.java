cddd20f652ff61774ae8aab8d60ada347928215e
IMWoo94 <leemeawoo94@gmail.com>
2023-12-08 19:55:58 +0900

    커넥션 풀 초과 API 생성 - 그라파나 모니터링 테스트용

--- src/main/java/toy/login/controller/TrafficController.java
+++ src/main/java/toy/login/controller/TrafficController.java
@@ -1,8 +1,13 @@
 package toy.login.controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +37,17 @@ public class TrafficController {
 		for (int i = 0; i < 1000000; i++) {
 			list.add("hello jvm!" + i);
 		}
+		return "ok";
+	}
+
+	@Autowired
+	DataSource dataSource;
+
+	@GetMapping("/jdbc")
+	public String jdbc() throws SQLException {
+		log.info("jdbc");
+		Connection conn = dataSource.getConnection();
+		log.info("connection info={}", conn); //conn.close();//커넥션을 닫지 않는다.
 		return "ok";
 	}
 }
