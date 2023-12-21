5f69d62e85210208ef1e2c2500223c4b96574b6b
IMWoo94 <leemeawoo94@gmail.com>
2023-12-21 19:59:06 +0900

    refactor : 로그인 Cookie 확인 방식 변경
    기존의 HttpServletRequest 에서 Cookies를 가져와 하나하나 찾아보는 방식에서
    @CookieValue 어노테이션을 사용해서 Controller 에 값이 바인딩 될 때 가져올 수 있도록 변경

--- src/main/java/toy/login/users/controller/UserController.java
+++ src/main/java/toy/login/users/controller/UserController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,11 +47,14 @@ public class UserController {
 		return new ResponseEntity<>(userService.findUserPassword(name, birthDate, loginId), HttpStatus.OK);
 	}
 
-	@GetMapping("/login/cookie")
-	public ResponseEntity loginCookie(@RequestParam String loginId, @RequestParam String password,
-		HttpServletResponse response, HttpServletRequest request) {
+	@GetMapping("/login/cookie/v1")
+	public ResponseEntity loginCookieV1(@RequestParam String loginId,
+		@RequestParam String password,
+		HttpServletResponse response,
+		HttpServletRequest request
+	) {
 
-		// httpRequest 에서 넘어온 로그인 관련 쿠기 정보가 있는지 확인
+		// httpRequest 에서 넘어온 로그인 관련 쿠키 정보가 있는지 확인
 		boolean isCookie = false;
 		Cookie[] cookies = request.getCookies();
 		for (Cookie cookie : cookies) {
@@ -74,6 +78,39 @@ public class UserController {
 		// 쿠키에 시간 정보를 주지 않으면 세션 쿠키 ( 브라우저 종료 시 모두 종료 )
 		Cookie idCookie = new Cookie("userLoginResult", "Y");
 		response.addCookie(idCookie);
+
+		return new ResponseEntity<>("로그인 되었습니다.", HttpStatus.OK);
+	}
+
+	@GetMapping("/login/cookie/v2")
+	public ResponseEntity loginCookieV2(@RequestParam String loginId,
+		@RequestParam String password,
+		HttpServletResponse response,
+		@CookieValue(name = "userLoginResult", required = false) String userLoginResult
+	) {
+
+		/**
+		 * @CookieValue(name = "userLoginResult", required = false)
+		 * Cookie 명이 userLoginResult 것을 찾고 없을 수도 있기 때문에 required = false 처리
+		 */
+
+		/**
+		 * httpRequest 를 받아서 하면 request 에 담겨 있는 모든 쿠키에 대해서 원하는 쿠키 정보가 있는지 확인을 해야 한다.
+		 * 이를 편하게 하기 위해서 @CookieValue 어노테이션을 사용하면 일치하는 Cookie 에 대해서 Controller 에 넘어오기 전에 값을 가져올 수 있다.
+		 */
+		// 쿠키 정보가 없거나, 쿠키 값이 아니라면 로그인 성공 여부에 따라 쿠키 정보 등록
+		if (userLoginResult == null || !"Y".equals(userLoginResult)) {
+			// 사용자로 부터 ID 와 PW 를 받는다. -> 일단은 HTTP 쿼리 파라미터로 제공
+			// 파라미터를 제공하지 않으면 ExceptionHandler 에 의해서 400 에러 발생
+			userService.login(loginId, password);
+
+			// 쿠키에 시간 정보를 주지 않으면 세션 쿠키 ( 브라우저 종료 시 모두 종료 )
+			Cookie idCookie = new Cookie("userLoginResult", "Y");
+			response.addCookie(idCookie);
+		} else {
+			// 쿠키가 존재하면 자동 로그인 처리
+			return new ResponseEntity<>("기 로그인되어 자동 로그인 되었습니다.", HttpStatus.OK);
+		}
 
 		return new ResponseEntity<>("로그인 되었습니다.", HttpStatus.OK);
 	}
