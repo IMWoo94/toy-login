462931abcf59d0dcfa18cf586b246c1e28f08804
IMWoo94 <leemeawoo94@gmail.com>
2023-12-24 13:12:28 +0900

    feat : redistemplate 를 사용한 로그인 기능 ( 세션 )
    로그인 세션 정보를 redisTemplate 를 통해서 저장

---
+++ src/main/java/toy/login/session/controller/LoginSessionRedisTemplateController.java
@@ -0,0 +1,42 @@
+package toy.login.session.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import toy.login.users.service.UserService;
+
+@RestController
+@Slf4j
+@RequestMapping("/session/template")
+@RequiredArgsConstructor
+public class LoginSessionRedisTemplateController {
+
+	private final RedisTemplate<String, String> redisTemplate;
+	private final UserService userService;
+
+	@GetMapping("/login")
+	public ResponseEntity login(@RequestParam String loginId,
+		@RequestParam String password,
+		HttpServletRequest request
+	) {
+
+		userService.login(loginId, password);
+
+		HttpSession session = request.getSession();
+
+		ValueOperations<String, String> operations = redisTemplate.opsForValue();
+		operations.set(session.getId(), "mySessionId");
+
+		return new ResponseEntity("로그인 되었습니다.", HttpStatus.OK);
+	}
+}
