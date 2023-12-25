1604cf23a5310bce9a570b12c54f9eed98d74e16
IMWoo94 <leemeawoo94@gmail.com>
2023-12-25 19:20:53 +0900

    refactor : naver controller 변경

--- src/main/java/toy/login/oauth/NaverLoginController.java
+++ src/main/java/toy/login/oauth/NaverLoginController.java
@@ -1,23 +1,21 @@
 package toy.login.oauth;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
-@RestController
+@Controller
 @RequestMapping("/auth/naver")
-@Slf4j
 public class NaverLoginController {
 
-	@GetMapping("/callback")
-	public ResponseEntity loginCallback(@RequestParam("code") String code, @RequestParam("state") String state) {
-		log.info("code : {}", code);
-		log.info("state : {}", state);
-		return new ResponseEntity("Naver 인증 성공", HttpStatus.OK);
+	@GetMapping
+	public String naverLogin() {
+		return "naver/APIExamNaverLogin.html";
 	}
+
+	@GetMapping("/callback")
+	public String naverLoginCallback() {
+		return "naver/callback.html";
+	}
+
 }
