f77b627676ec387e16b46bd036c245e885eec801
IMWoo94 <leemeawoo94@gmail.com>
2023-12-29 17:34:48 +0900

    refactor : 불필요 객체 제거

--- src/main/java/toy/login/commons/config/oauth/SecurityConfig.java
+++ src/main/java/toy/login/commons/config/oauth/SecurityConfig.java
@@ -5,14 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.RequiredArgsConstructor;
-import toy.login.oauth.service.CustomOAuth2UserService;
-
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-	private final CustomOAuth2UserService customOAuth2UserService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
